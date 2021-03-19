fa75787e623d95192907fd368c86109494354003
Sirenoman <00003120@uca.edu.sv>
2021-03-19 13:53:17 -0600

    Add optative files

---
+++ cicloAnidado/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace cicloAnidado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a Optativa de Ciclos Anidados");
+            Console.Write("Ingrese la cantidad de numeros de la serie: ");
+            int total = Int32.Parse(Console.ReadLine());
+            double average = 0;
+
+            bool follow = true;
+            // funcion While para preguntar al usuario si seguir sacando promedios
+            while (follow)
+            {
+                Console.WriteLine($"Cantidad de la serie: {total}");
+                // Funcion for para leer, sumar y promediar los numeros 
+                for(int i = 1; i <= total; i++)
+                {
+                    Console.Write($"#{i} Numero: ");
+                    average += Double.Parse(Console.ReadLine());
+                }
+                var aDouble = average/total;
+                Console.WriteLine($"El promedio final es: {aDouble}");
+
+                bool flag = true;
+                // Otro While anidado para consultar si seguir y que se tome en cuenta si es la respuesta correta el 's' o 'n'
+                while(flag)
+                {
+                    Console.WriteLine("¿Quieres seguir calculando mas promedios? s/n");
+                    var value = Char.Parse(Console.ReadLine());
+                    if( value == 'n' || value == 'N')
+                    {
+                        // si escribe N o n entonces el primer y segundo While se terminaran
+                        flag = false;
+                        follow = false;
+                    }else if(value == 's' || value == 'S')
+                    {
+                        flag = false;
+                    }
+                }
+        
+            }
+
+        }
+    }
+}
---
+++ optativeStrct/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+struct Date{
+    public int year, month, day;
+}
+
+namespace optativeStrct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Date birthday;
+            Console.WriteLine("Favor ingresar tu fecha de cumpleaños (en numeracion)");
+            Console.Write("Año: ");
+            birthday.year = ItRealYear();
+            Console.Write("Mes: ");
+            birthday.month = ItRealMonth();
+            Console.Write("Dia: ");
+            birthday.day= ItRealDay(birthday.month);
+
+            Date oneDate;
+            Console.WriteLine("Favor ingresar la fecha de ahora (en numeracion)");
+            Console.Write("Año: ");
+            oneDate.year = ItRealYear();
+            Console.Write("Mes: ");
+            oneDate.month = ItRealMonth();
+            Console.Write("Dia: ");
+            oneDate.day= ItRealDay(oneDate.month);
+
+            if(birthday.day == oneDate.day && birthday.month == oneDate.month)
+            {
+                Console.WriteLine("¡FELICIDADES! Hoy es tu dia, celebralo a lo grande");
+            }
+            else
+            {
+                Console.WriteLine("Hoy no es tu cumpleaños :c");
+            }
+        }
+
+        // metodos para validar el año, el mes y el dia correctamente acorde al calendario.
+        static int ItRealYear()
+        {
+            bool flag = true;
+            int y = 0;
+            while(flag)
+            {
+                y = Int32.Parse(Console.ReadLine());
+                if( y > 0)
+                {
+                    flag = false;
+                }else
+                {
+                    Console.WriteLine("error!");
+                    Console.Write("Ingrese un año correcto: ");
+                }
+            }
+            return y;
+                
+        }
+        // evaluar mes
+        static int ItRealMonth()
+        {
+            bool flag = true;
+            int m = 0;
+            while(flag)
+            {
+                m = Int32.Parse(Console.ReadLine());
+                if( m > 0 && m < 13)
+                {
+                    flag = false;
+                }else
+                {
+                    Console.WriteLine("error!");
+                    Console.Write("Ingrese un mes correcto: ");
+                }
+            }
+            return m;
+        }
+        // evaluar dia
+        static int ItRealDay(int mth)
+        {
+            bool flag = true;
+            int d = 0;
+            while(flag)
+            {
+                d = Int32.Parse(Console.ReadLine());
+                if( mth == 2)
+                {
+                    if(d > 0 && d < 28)
+                        flag = false;
+                    else
+                        Console.Write("Error! ingrese un dia correcto: ");
+                }
+                else if(mth == 1 || mth == 3 || mth == 5 || mth  == 7 || mth == 8 || mth == 10 || mth == 12)
+                {
+                    if(d > 0 && d < 32)
+                        flag = false;
+                    else
+                        Console.Write("Error! ingrese un dia correcto: ");
+                }
+                else if(mth == 4 || mth == 6 || mth == 9 || mth == 11)
+                {
+                    if(d > 0 && d < 31)
+                        flag = false;
+                    else
+                        Console.Write("Error! ingrese un dia correcto: ");
+                }
+            }
+            return d;
+        }
+    }
+}
