523b7393433e393ad28d6bc84cc3329515955910
Bryaan159 <00057720@uca.edu.sv>
2021-03-20 23:34:30 -0600

    Comment the code

--- cicloAnidado/Program.cs
+++ cicloAnidado/Program.cs
@@ -6,34 +6,37 @@ namespace cicloAnidado
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bienvenido a Optativa de Ciclos Anidados");
-            Console.Write("Ingrese la cantidad de numeros de la serie: ");
+            Console.WriteLine("Welcome to Nested Cycles");
+            Console.Write("Enter the number of numbers in the series: ");
+            
+            //Declaration of variables
             int total = Int32.Parse(Console.ReadLine());
             double average = 0;
-
             bool follow = true;
-            // funcion While para preguntar al usuario si seguir sacando promedios
+            
+            //Funtion works to ask user if it should keep taking averages
             while (follow)
             {
-                Console.WriteLine($"Cantidad de la serie: {total}");
+                Console.WriteLine($"Series quantity: {total}");
                 // Funcion for para leer, sumar y promediar los numeros 
                 for(int i = 1; i <= total; i++)
                 {
-                    Console.Write($"#{i} Numero: ");
+                    Console.Write($"#{i} Number: ");
                     average += Double.Parse(Console.ReadLine());
                 }
                 var aDouble = average/total;
-                Console.WriteLine($"El promedio final es: {aDouble}");
+                Console.WriteLine($"The final average is: {aDouble}");
 
+                //Function to check if it should be repeated
                 bool flag = true;
-                // Otro While anidado para consultar si seguir y que se tome en cuenta si es la respuesta correta el 's' o 'n'
                 while(flag)
-                {
-                    Console.WriteLine("¿Quieres seguir calculando mas promedios? s/n");
+                {   
+                    //if you type N or n then the first and second While will be terminated
+                    Console.WriteLine("Do you want to calcute the average again? s/n");
                     var value = Char.Parse(Console.ReadLine());
                     if( value == 'n' || value == 'N')
                     {
-                        // si escribe N o n entonces el primer y segundo While se terminaran
+                        
                         flag = false;
                         follow = false;
                     }else if(value == 's' || value == 'S')
